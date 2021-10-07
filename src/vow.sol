59dd079dee883d0cd391425b29dbca71b0a5be76
nikolai <mail@nikolai.fyi>
2021-10-07 16:41:59 -0400

    vow sketch

--- src/vow.sol
+++ src/vow.sol
@@ -1,155 +1,69 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-/// vow.sol -- Dai settlement module
-
-// Copyright (C) 2018 Rain <rainbreak@riseup.net>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 pragma solidity 0.8.6;
 
-import './mixin/ward.sol';
-
-// FIXME: This contract was altered compared to the production version.
-// It doesn't use LibNote anymore.
-// New deployments of this contract will need to include custom events (TO DO).
-
-interface FlopLike {
-    function kick(address gal, uint lot, uint bid) external returns (uint);
-    function cage() external;
-    function live() external returns (uint);
-}
-
-interface FlapLike {
-    function kick(uint lot, uint bid) external returns (uint);
-    function cage(uint) external;
-    function live() external returns (uint);
-}
-
 interface VatLike {
-    function dai (address) external view returns (uint);
-    function sin (address) external view returns (uint);
-    function heal(uint256) external;
-    function hope(address) external;
-    function nope(address) external;
+  function dai() external returns (uint);
+  function vice() external returns (uint);
+  function heal(uint amt) external;
+  function drip(bytes32 ilk) external;
 }
 
-contract Vow is Ward {
-    // --- Data ---
-    VatLike public vat;        // CDP Engine
-    FlapLike public flapper;   // Surplus Auction House
-    FlopLike public flopper;   // Debt Auction House
+interface BPool {
+    function swap_exactAmountIn(address gem, uint amt) external returns (uint);
+    function swap_exactAmountOut(address gem, uint amt) external returns (uint);
+    function view_exactAmountOut(address gem, uint amt) external returns (uint);
+}
 
-    mapping (uint256 => uint256) public sin;  // debt queue
-    uint256 public Sin;   // Queued debt            [rad]
-    uint256 public Ash;   // On-auction debt        [rad]
+interface DaiJoin {
+    function exit(uint amt) external;
+    function join(uint amt) external;
+}
 
-    uint256 public wait;  // Flop delay             [seconds]
-    uint256 public dump;  // Flop initial lot size  [wad]
-    uint256 public sump;  // Flop fixed bid size    [rad]
+interface GemLike {
+    function mint(address usr, uint amt) external;
+    function burn(address usr, uint amt) external;
+    function approve(address usr, uint amt) external;
+    function balanceOf(address usr) external returns (uint);
+}
 
-    uint256 public bump;  // Flap fixed lot size    [rad]
-    uint256 public hump;  // Surplus buffer         [rad]
+contract Vow {
+    VatLike public vat;
+    DaiJoin public daijoin;
+    GemLike public RICO;
+    GemLike public BANK;
+    BPool public pool;
 
-    uint256 public live;  // Active Flag
-
-    // --- Init ---
-    constructor(address vat_, address flapper_, address flopper_) public {
-        vat     = VatLike(vat_);
-        flapper = FlapLike(flapper_);
-        flopper = FlopLike(flopper_);
-        vat.hope(flapper_);
-        live = 1;
-    }
-
-    // --- Math ---
-    function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x);
-    }
-    function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x);
-    }
-    function min(uint x, uint y) internal pure returns (uint z) {
-        return x <= y ? x : y;
-    }
-
-    // --- Administration ---
-    function file(bytes32 what, uint data) external auth {
-        if (what == "wait") wait = data;
-        else if (what == "bump") bump = data;
-        else if (what == "sump") sump = data;
-        else if (what == "dump") dump = data;
-        else if (what == "hump") hump = data;
-        else revert("Vow/file-unrecognized-param");
-    }
-
-    function file(bytes32 what, address data) external auth {
-        if (what == "flapper") {
-            vat.nope(address(flapper));
-            flapper = FlapLike(data);
-            vat.hope(data);
+    function drip(bytes32[] calldata ilks) public {
+        for(uint i = 0; i < ilks.length; i++) {
+            vat.drip(ilks[i]);
         }
-        else if (what == "flopper") flopper = FlopLike(data);
-        else revert("Vow/file-unrecognized-param");
+        uint dai = vat.dai();
+        daijoin.exit(dai);
     }
 
-    // Push to debt-queue
-    function fess(uint tab) external auth {
-        sin[block.timestamp] = add(sin[block.timestamp], tab);
-        Sin = add(Sin, tab);
-    }
-    // Pop from debt-queue
-    function flog(uint era) external {
-        require(add(era, wait) <= block.timestamp, "Vow/wait-not-finished");
-        Sin = sub(Sin, sin[era]);
-        sin[era] = 0;
+    // sell surplus rico for bank, burn bank
+    function flap() public {
+        uint rico = RICO.balanceOf(address(this));
+        uint bank = pool.swap_exactAmountIn(address(RICO), rico);
+        BANK.burn(address(this), bank);
     }
 
-    // Debt settlement
-    function heal(uint rad) external {
-        require(rad <= vat.dai(address(this)), "Vow/insufficient-surplus");
-        require(rad <= sub(sub(vat.sin(address(this)), Sin), Ash), "Vow/insufficient-debt");
-        vat.heal(rad);
-    }
-    function kiss(uint rad) external {
-        require(rad <= Ash, "Vow/not-enough-ash");
-        require(rad <= vat.dai(address(this)), "Vow/insufficient-surplus");
-        Ash = sub(Ash, rad);
-        vat.heal(rad);
+    function flop() public {
+        uint vice = vat.vice();
+        uint need = pool.view_exactAmountOut(address(RICO), vice);
+        BANK.mint(address(this), need);
+        pool.swap_exactAmountOut(address(RICO), vice);
+        daijoin.join(vice);
+        vat.heal(vice);
     }
 
-    // Debt auction
-    function flop() external returns (uint id) {
-        require(sump <= sub(sub(vat.sin(address(this)), Sin), Ash), "Vow/insufficient-debt");
-        require(vat.dai(address(this)) == 0, "Vow/surplus-not-zero");
-        Ash = add(Ash, sump);
-        id = flopper.kick(address(this), dump, sump);
-    }
-    // Surplus auction
-    function flap() external returns (uint id) {
-        require(vat.dai(address(this)) >= add(add(vat.sin(address(this)), bump), hump), "Vow/insufficient-surplus");
-        require(sub(sub(vat.sin(address(this)), Sin), Ash) == 0, "Vow/debt-not-zero");
-        id = flapper.kick(bump, 0);
-    }
-
-    function cage() external auth {
-        require(live == 1, "Vow/not-live");
-        live = 0;
-        Sin = 0;
-        Ash = 0;
-        flapper.cage(vat.dai(address(flapper)));
-        flopper.cage();
-        vat.heal(min(vat.dai(address(this)), vat.sin(address(this))));
+    function reapprove() public {
+        RICO.approve(address(daijoin), type(uint256).max);
+        RICO.approve(address(pool), type(uint256).max);
+        BANK.approve(address(pool), type(uint256).max);
     }
 }
+
+
+
