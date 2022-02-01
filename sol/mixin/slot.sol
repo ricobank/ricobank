d624b71a1d1d4912c2d0ceaf2a8bebff37db6602
nikolai <mail@nikolai.fyi>
2022-02-01 07:13:35 -0400

    add slot.sol

---
+++ sol/mixin/slot.sol
@@ -0,0 +1,16 @@
+/// SPDX-License-Identifier: AGPL-3.0
+
+// Direct read access for any storage slot
+
+pragma solidity 0.8.9;
+
+contract Slot {
+    function _slot(bytes32 arg) internal pure returns (bytes32 ret) {
+        assembly {
+	    ret := mload(arg)
+	}
+    }
+    function slot(bytes32 arg) external pure returns (bytes32 ret) {
+        return _slot(arg);
+    }
+}
