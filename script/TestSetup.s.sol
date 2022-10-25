7fd380e65e314854dd81c6a37239a741573cc9ec
stobiewan <getsforwarded@gmail.com>
2022-10-26 00:01:24 +1300

    foundry script for easier testing on local persistent networks (#189)

---
+++ script/TestSetup.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.17;
+
+import "forge-std/Script.sol";
+import { RicoSetUp } from "../test/RicoHelper.sol";
+
+contract SetupScript is Script, RicoSetUp {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        make_bank();
+        console.log('vat address is %s', avat);
+        vm.stopBroadcast();
+    }
+}
