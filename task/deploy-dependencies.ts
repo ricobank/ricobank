9715b942771629e952c963850f7b3b0428392504
kbrav <kevin@halys.dev>
2024-01-28 03:07:33 -0500

    add packs to ipfs

--- task/deploy-dependencies.ts
+++ task/deploy-dependencies.ts
@@ -5,14 +5,23 @@ const dpack = require('@etherpacks/dpack')
 
 task('deploy-dependencies', '')
 .addOptionalParam('gasLimit', 'per-tx gas limit')
+.addOptionalParam('ipfs', 'add packs to ipfs')
 .addOptionalParam('mock', 'mock mode')
 .setAction(async (args, hre) => {
   debug('deploying dependencies...')
-  const uni_pack = require(`../lib/uniswapv3/pack/uniswapv3_${args.netname}.dpack.json`)
+
+  let uni_pack
+  if (args.unipackcid) {
+    uni_pack = await dpack.getIpfsJson(args.unipackcid)
+  } else {
+    uni_pack = require(`../lib/uniswapv3/pack/uniswapv3_${args.netname}.dpack.json`)
+  }
   uni_pack.network = hre.network.name
   debug('found uni pack')
+
   const fb_pack = await hre.run('deploy-feedbase', {netname: args.netname})
   debug('deployed fb')
+
   let gf_pack
   if (args.gfpackcid) {
     gf_pack = await dpack.getIpfsJson(args.gfpackcid)
@@ -20,7 +29,7 @@ task('deploy-dependencies', '')
     gf_pack = await hre.run(
       'deploy-gemfab', {netname: args.netname, gasLimit: args.gasLimit}
     )
-    debug('deployed gf')
+    debug(`deployed gf`)
   }
 
   const tokens_pack = await hre.run(
@@ -43,6 +52,20 @@ task('deploy-dependencies', '')
 
   const pack = await pb.build();
   pack.network = hre.network.name
+
+  if (args.ipfs) {
+    console.log("deploy-dependencies IPFS CIDs:")
+    let cid = await dpack.putIpfsJson(gf_pack, true)
+    console.log(`  GemFab: ${cid}`)
+    cid = await dpack.putIpfsJson(fb_pack, true)
+    console.log(`  Feedbase: ${cid}`)
+    cid = await dpack.putIpfsJson(uni_pack, true)
+    console.log(`  UniswapV3: ${cid}`)
+    cid = await dpack.putIpfsJson(tokens_pack, true)
+    console.log(`  Tokens: ${cid}`)
+    cid = await dpack.putIpfsJson(pack, true)
+    console.log(`  Dependencies: ${cid}`)
+  }
 
   return pack
 });
